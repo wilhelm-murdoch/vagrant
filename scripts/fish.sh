5d616ffd22866a30aff19d1ca06f36af9a8670c1
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2014-06-24 03:53:38 +1000

    dwm installed

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -1,3 +1,5 @@
+rm -R /home/terry
+
 pacman -Syy
 pacman -Su
 pacman -S alsa-utils xorg-server xorg-server-utils xorg-xinit virtualbox-guest-utils xorg-twm xorg-xclock xterm --noconfirm
@@ -5,7 +7,9 @@ modprobe -a vboxguest vboxsf
 
 pacman -S base-devel abs dmenu --noconfirm
 abs community/dwm
-cp -r /var/abs/community/dwm ~/dwm
-cd ~/dwm
-makepkg -i
+cp -r /var/abs/community/dwm /home/vagrant/dwm
+cd /home/vagrant/dwm
+makepkg -i --asroot --noconfirm
+cd ..
 echo "exec dwm" > .xinitrc
+chown -R vagrant:vagrant /home/vagrant
